458ea633e355029b91be3ee7eab0f7ae88903737
Sven <sven.dupli@gmail.com>
2013-03-11 16:09:23 +0100

    World object; neighbours detected to north

--- gol.rb
+++ gol.rb
@@ -1,14 +1,34 @@
 class Cell
 
-  attr_accessor :x, :y
+  attr_accessor :world, :x, :y
 
-  def initialize(x=0, y=0)
+  def initialize(world=World.new, x=0, y=0)
+    @world = world
+    @world.cells << self
     @x = x
     @y = y
   end
 
   def neighbours
-    []
+    @neighbours = []
+    world.cells.each do |cell|
+
+      # Detects neighbour to the north
+      if self.x == cell.x && self.y == cell.y - 1
+        @neighbours << cell
+      end
+
+    end
+    @neighbours
   end
 
+end
+
+class World
+  attr_accessor :cells
+
+  def initialize
+    @cells = []
+  end
+  
 end
--- gol_spec.rb
+++ gol_spec.rb
@@ -1,20 +1,41 @@
 require 'rspec'
 require_relative 'gol.rb'
   
-describe 'game of life' do
+describe 'Game of life' do
 
-  context 'Cell utility methods' do
-    it 'spawn new cell at coordinates' do
-      cell = Cell.new(3, 5)
-      cell.is_a?(Cell).should be_true
-      cell.x.should == 3
-      cell.y.should == 5
+  context 'Cell' do
+    subject { Cell.new }
+
+    it 'Creates new cell properly' do
+      subject.x = 3
+      subject.y = 5
+      subject.is_a?(Cell).should be_true
+      subject.world.is_a?(World).should be_true
+      subject.x.should == 3
+      subject.y.should == 5
+      subject.world.cells.count.should == 1
+    end
+
+    it 'Detects cell to the north' do
+      neighbour_cell = Cell.new(subject.world, 0, 1)
+      subject.neighbours.count.should == 1
+    end
+
+  end
+
+  context 'World' do
+    subject { World.new }
+
+    it 'Has empty cells array upon initialization' do
+      subject.cells.count.should == 0
     end
   end
 
-  it '1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
-    cell = Cell.new
-    cell.neighbours.count.should == 0
+  context 'Rules' do
+    it '1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+      cell = Cell.new
+      cell.neighbours.count.should == 0
+    end
   end
 
 end
