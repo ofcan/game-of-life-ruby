2f20c042af2b8324c5f44dd13a1f69cc4446998c
Sven <sven.dupli@gmail.com>
2013-03-11 13:39:04 +0100

    Added color to rspec; added 1st failing test

--- gol_spec.rb
+++ gol_spec.rb
@@ -2,7 +2,9 @@ require 'rspec'
 
 describe 'game of life' do
 
-  it '1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.'
+  it '1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.' do
+    cell = Cell.new
+    cell.neighbours_count.should == 0
   end
 
 end
