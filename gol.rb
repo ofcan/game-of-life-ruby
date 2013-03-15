e6ca9575eb539052c5dd16b9fa8602047e255e9f
Sven <sven.dupli@gmail.com>
2013-03-15 09:46:19 +0100

    Made game window smaller and stored neighbours count in tick! method and now the game works! But not optimised enough...

--- gol.rb
+++ gol.rb
@@ -12,25 +12,26 @@ class Game
     next_round_live_cells = []
     next_round_dead_cells = []
 
-    world.cells.each do |cell|
+    @world.cells.each do |cell|
+      neighbour_count = world.live_neighbours_around_cell(cell).count
       # Rule 1: 
       # Any live cell with fewer than two live neighbours dies
-      if cell.alive? && world.live_neighbours_around_cell(cell).count < 2
+      if cell.alive? && neighbour_count < 2
         next_round_dead_cells << cell
       end
       # Rule 2:
       # Any live cell with two or three live neighbours lives on to the next generation
-      if cell.alive? && world.live_neighbours_around_cell(cell).count == (2 || 3)
+      if cell.alive? && neighbour_count == (2 || 3)
         next_round_live_cells << cell
       end
       # Rule 3:
       # Any live cell with more than three live neighbours dies
-      if cell.alive? && world.live_neighbours_around_cell(cell).count > 3
+      if cell.alive? && neighbour_count > 3
         next_round_dead_cells << cell
       end
       # Rule 4:
       # Any dead cell with exactly three live neighbours becomes a live cell
-      if cell.dead? && world.live_neighbours_around_cell(cell).count == 3
+      if cell.dead? && neighbour_count == 3
         next_round_live_cells << cell
       end
     end
--- gosu.rb
+++ gosu.rb
@@ -3,12 +3,12 @@ require_relative 'gol.rb'
 
 class GameOfLifeWindow < Gosu::Window
 
-  def initialize(height=800, width=600)
+  def initialize(height=300, width=300)
 
     # Basics
     @height = height
     @width = width
-    super height, width, false, 500
+    super height, width, false
     self.caption = 'My Game of Life'
 
     # Colors
@@ -18,8 +18,8 @@ class GameOfLifeWindow < Gosu::Window
     # Game world
     @rows = height/10
     @cols = width/10
-    world = World.new(@cols, @rows)
-    @game = Game.new(world)
+    @world = World.new(@cols, @rows)
+    @game = Game.new(@world)
     @row_height = height/@rows
     @col_width = width/@cols
     @game.world.randomly_populate
@@ -28,19 +28,20 @@ class GameOfLifeWindow < Gosu::Window
   end
 
   def update
-#    unless @game.world.live_cells.count == 0
-#      @game.tick!
+    unless @game.world.live_cells.count == 0
+      @game.tick!
       @generation += 1
-#    end
+    end
   end
 
   def draw
+    draw_background
     @game.world.cells.each do |cell|
       if cell.alive?
-        draw_quad(cell.x * @col_width, cell.y * @row_height, @black,
-                  cell.x * @col_width + @col_width, cell.y * @row_height, @black,
-                  cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @black,
-                  cell.x * @col_width, cell.y * @row_height + @row_height, @black)
+        draw_quad(cell.x * @col_width, cell.y * @row_height, @white,
+                  cell.x * @col_width + @col_width, cell.y * @row_height, @white,
+                  cell.x * @col_width + @col_width, cell.y * @row_height + @row_height, @white,
+                  cell.x * @col_width, cell.y * @row_height + @row_height, @white)
       end
     end
   end
@@ -54,11 +55,15 @@ class GameOfLifeWindow < Gosu::Window
     end
   end
 
+  def needs_cursor?
+    true
+  end
+
   def draw_background
-    draw_quad(0, 0, @white,
-              width, 0, @white,
-              width, height, @white,
-              0, height, @white)
+    draw_quad(0, 0, @black,
+              width, 0, @black,
+              width, height, @black,
+              0, height, @black)
   end
 
 end
